e74b5df9729491f95972e1c2056541fe77341a36
Evan Liu <evanyxliu@gmail.com>
2025-11-01 00:49:59 -0400

    fixed gpio example

--- examples/gpio/main.c
+++ examples/gpio/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "gpio.h"
+#include "drivers/gpio.h"
 
 #define LED_PIN 25
 
@@ -8,7 +8,7 @@ int main() {
     stdio_init_all(); 
     gpio_config(LED_PIN, GPIO_OUTPUT);
 
-    while (true) {
+    while (1) {
         gpio_write(LED_PIN, 1);
         sleep_ms(500);
         gpio_write(LED_PIN, 0);
